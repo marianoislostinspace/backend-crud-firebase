25adf4a08dacc6d86d61e0a200385711c689d862
mariano ferreyra <marianoislostinspace@gmail.com>
2025-09-11 17:12:30 -0300

    error fixed

--- src/config/jwt.config.js
+++ src/config/jwt.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || process.env.JWBPASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET || process.env.JWTPASSWORD,
   JWT_EXPIRES: "2h"
 };
