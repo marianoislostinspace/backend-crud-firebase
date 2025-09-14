be41f77cc14af2d0e93308d4d68fd21c93246b48
mariano ferreyra <marianoislostinspace@gmail.com>
2025-09-13 21:34:13 -0300

    añadimos mas usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,18 +2,32 @@ const jwt = require("jsonwebtoken")
 const { JWT_SECRET, JWT_EXPIRES } = require("../config/jwt.config");
 require('dotenv').config();
 
-const USERNAME = process.env.USERNAME
-const PASSWORD = process.env.PASSWORD
+const USERS = [
+    { username: process.env.USERNAME1, password: process.env.PASSWORD1 },
+    { username: process.env.USERNAME2, password: process.env.PASSWORD2 },
+    { username: process.env.USERNAME3, password: process.env.PASSWORD3 },
+]
 
- const login = (req, res) => {
+
+
+const login = (req, res) => {
     const { username, password } = req.body
 
-    if (username === USERNAME && password === PASSWORD) {
-        const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
-        return res.json({ token })
+    const match = USERS.find(
+        u => u.username === username && u.password === password
+    )
+
+    if (!match) {
+        return res.status(401).json({ message: "Credenciales ivalidas Chaval" });
+
     }
 
-    return res.status(401).json({ message: "Credenciales ivalidas Chaval" });
+
+
+    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    return res.json({ token })
+
+
 }
 
 module.exports = login
