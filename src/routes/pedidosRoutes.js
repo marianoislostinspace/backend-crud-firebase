41b0d974cfb0eb2c99cf7822a998430043307e93
mariano ferreyra <marianoislostinspace@gmail.com>
2025-09-21 19:10:47 -0300

    chancho2

--- src/routes/pedidosRoutes.js
+++ src/routes/pedidosRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pedidosController = require('../controllers/pedidosController');
+const authMiddleware = require("../middlewares/authMiddleware")
 
 module.exports = function (io) {
 
@@ -8,7 +9,7 @@ module.exports = function (io) {
     const router = express.Router();
     const { obtenerPedidos, agregarPedido, EliminarPedido } = pedidosController(io);
     
-    router.use()
+    router.use(authMiddleware)
     //ruta para obtener los pedidos
     router.get('/', obtenerPedidos);
 
